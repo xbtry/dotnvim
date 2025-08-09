34d80857ec904cd105f4a977a6690e4a00c95c0c
xbtry <xbaturayx@outlook.com>
2025-08-09 11:52:39 +0300

    first

---
+++ lua/core/options.lua
@@ -0,0 +1,13 @@
+vim.wo.number = true
+vim.o.relativenumber = true
+vim.o.clipboard = "unnamedplus"
+vim.o.wrap = true
+vim.o.linebreak = true
+vim.o.autoindent = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.shiftwidth = 4
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.cursorline = true
+vim.opt.termguicolors = true
